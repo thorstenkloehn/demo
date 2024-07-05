b2df760d5e4bb4c456f2eb67f490efe6bcaf2edc
Thorsten Klöhn <thorstenkloehn@gmail.com>
2024-07-05 20:24:40 +0200

    start

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 443;
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Server listening on port ${port}`);
 });
