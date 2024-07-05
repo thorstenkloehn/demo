8076b8214597f821138f41ea61a71704ce698e7c
Thorsten Klöhn <thorstenkloehn@gmail.com>
2024-07-05 19:57:59 +0200

    start

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Verwenden Sie die Umgebungsvariable PORT oder einen Standardwert
+const port = process.env.PORT || 3000;
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server läuft auf Port ${port}`);
+});
