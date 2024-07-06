854e2d6ff1d1faf6577613ee3d5a7c02ea477342
Thorsten Klöhn <thorstenkloehn@gmail.com>
2024-07-07 01:16:24 +0200

    start

---
+++ Post.js
@@ -0,0 +1,10 @@
+class Post {
+    constructor(id, titel, inhalt, datum) {
+      this.Id = id;
+      this.Titel = titel;
+      this.Inhalt = inhalt;
+      this.Datum = datum;
+    }
+  }
+  
+  module.exports = Post;
--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const app = express();
-let port;
+const express = require('express'); // Import express
+const app = express(); // Create an express app
+const  port = process.env.PORT;
+const Post = require('./Post');
 
-  port = process.env.PORT;
-
+// Beispiel für die Verwendung der Klasse
+const newPost = new Post(1, "Ahrensburg", "Ahrensburg ist eine Stadt im Bundesland Schleswig-Holstein. Sie liegt nordöstlich von Hamburg und gehört zur Metropolregion Hamburg. Ahrensburg ist die größte Stadt des Kreises Stormarn und hat etwa 32.000 Einwohner. Die Stadt ist bekannt für ihr Schloss, das im 16. Jahrhundert erbaut wurde. Ahrensburg hat eine gute Verkehrsanbindung an Hamburg und Lübeck. Die Stadt ist ein beliebtes Ausflugsziel für Touristen, die die schöne Landschaft und die historischen Gebäude der Stadt erkunden möchten.", "2021-09-01");
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
 
+  res.json(newPost);
+});
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
